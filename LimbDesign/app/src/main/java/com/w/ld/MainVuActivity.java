e34ec3d1845ca816eca328821b83b98f49241709
yutao <qq545057627@gmail.com>
2017-08-01 09:38:36 +0800

    提交项目主体

---
+++ LimbDesign/app/src/main/java/com/w/ld/MainVuActivity.java
@@ -0,0 +1,13 @@
+package com.w.ld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainVuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_vu);
+    }
+}
